81a66bf0014058f7eab4485342b698fe89dc3e8d
Yashwanth <yashwanthbabu.gujarathi@gmail.com>
2014-07-23 15:28:26 +0530

    Updated the CommentAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
     search_fields = ["title"]
+    display_fields = ["title", "created"]
+
+class CommentAdmin(admin.ModelAdmin):
+    display_fields = ["post", "author", "created"]
+
 
 admin.site.register(Post, PostAdmin)
-
+admin.site.register(Comment, CommentAdmin)
 
