c17e748136e959cdc6ed87c22ba7eed362a19b15
Yashwanth <yashwanth@agiliq.com>
2014-07-24 16:48:42 +0530

    Commit of adding monthly archives

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from django.core.context_processors import csrf
 
 from .models import Post, Comment
 from django.forms import ModelForm
+from blog.forms import CommentForm
 
 def post(request, post_id):
     """Single post with comments and a comment form."""
@@ -36,6 +37,39 @@ def add_comment(request, post_id):
         comment.author = author
         comment.save()
     return redirect("main")
+    
+def mkmonth_lst():
+    """Make a list of months to show archive links."""
+
+    if not Post.objects.count():
+        return []
+
+    # set up vars
+    year, month = time.localtime()[:2]
+    first = Post.objects.order_by("created")[0]
+    fyear = first.created.year
+    fmonth = first.created.month
+    months = []
+
+    # loop over years and months
+    for y in range(year, fyear-1, -1):
+        start, end = 12, 0
+        if y == year:
+            start = month
+        if y == fyear:
+            end = fmonth-1
+
+        for m in range(start, end, -1):
+            months.append((y, m, month_name[m]))
+    return months
+    
+def month(request, year, month):
+    """Monthly archive."""
+    
+    posts = Post.objects.filter(created__year=year, created__month=month)
+    return render_to_response("list.html", dict(post_list=posts, user=request.user, 
+                                                months=mkmonth_lst(), archive=True))
+
 
 def blog(request):
     """Main listing."""
@@ -52,4 +86,5 @@ def blog(request):
     except (InvalidPage, EmptyPage):
         posts = paginator.page(paginator.num_pages)
 
-    return render_to_response("list.html", dict(posts=posts, user=request.user))
+    return render_to_response("list.html", dict(posts=posts, user=request.user
+                                                post_list=posts.objects_list, months=mkmonth_lst()))
