924294f956cf6dce2d7c5588700ff4667aced98e
Yashwanth <yashwanth@agiliq.com>
2014-07-24 16:53:47 +0530

    Added url to month view.

--- sampleblog/urls.py
+++ sampleblog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^blog/', 'blog.views.blog', name='main'),
     url(r"post/(?P<post_id>[0-9]+)/$", 'blog.views.post', name='post'),
     url(r"^add_comment/(?P<post_id>[0-9]+)/$", 'blog.views.add_comment', name='add_comment'),
+    url(r"^month/(?P<year>\d{4})/(?P<month>(0|1)?\d)/$", 'blog.views.month', name='month'),
     # url(r'^$', 'sampleblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
