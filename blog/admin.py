d7f94e73b3085fa2cb7cbfa1c9bcba4d0e26bec2
Yashwanth <yashwanth@agiliq.com>
2014-07-24 15:42:03 +0530

    Commit for pep8 convention

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,11 @@ from .models import Post, Comment
 
 # Register your models here.
 
+
 class PostAdmin(admin.ModelAdmin):
     search_fields = ["title"]
     display_fields = ["title", "created"]
+
 
 class CommentAdmin(admin.ModelAdmin):
     display_fields = ["post", "author", "created"]
