37a721647f0f3035688fa971c0d5b521557eba9f
Yashwanth <yashwanth@agiliq.com>
2014-07-25 16:51:34 +0530

    Added urls to delete single and multi comments.

--- sampleblog/urls.py
+++ sampleblog/urls.py
@@ -9,17 +9,11 @@ urlpatterns = patterns('',
     url(r"post/(?P<post_id>[0-9]+)/$", 'blog.views.post', name='post'),
     url(r"^add_comment/(?P<post_id>[0-9]+)/$", 'blog.views.add_comment', name='add_comment'),
     url(r"^month/(?P<year>\d{4})/(?P<month>(0|1)?\d)/$", 'blog.views.month', name='month'),
-<<<<<<< HEAD
     url(r"^delete_comment/(?P<post_pk>[0-9]+)/$", 'blog.views.delete_comment', name='delete_comment'),
     url(r"^delete_comment/(?P<post_pk>[0-9]+)/(?P<comment_id>(0|1)?\d)/$", 'blog.views.delete_comment', name='delete_comment'),
     url(r"^aboutme/", 'blog.views.aboutme', name='aboutme'),
-=======
->>>>>>> 5234395f7ae24f76e99e8922cb315a3d7adbbb93
     # url(r'^$', 'sampleblog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
     url(r'^admin/', include(admin.site.urls)),
 )
-
-
-
