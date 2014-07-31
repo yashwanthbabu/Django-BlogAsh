353efd9bebae0c0cd44e8daad63642dcbf52b2f7
Yashwanth <yashwanth@agiliq.com>
2014-07-31 15:40:11 +0530

    Added CommentsForm and PostForm for recent blog entries

--- blog/forms.py
+++ blog/forms.py
@@ -5,3 +5,13 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         exclude = ["post"]
+        
+        
+class CommentsForm(forms.Form):
+    name = forms.CharField(max_length=32)
+    comment = forms.CharField(widget=forms.Textarea(attrs={'cols': 30, 'rows': 4}))
+
+
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=50)
+    body = forms.CharField(widget=forms.Textarea, required=False)
