97de5aa1a9f9c771d79ef037f1fe088180ea2134
Yashwanth <yashwanth@agiliq.com>
2014-08-04 12:43:40 +0530

    Added tests.py

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+from .views import post, add_comment, \
+    month, delete_comment, month, blog
 # Create your tests here.
+
+
+class BlogViewsTestCase(TestCase):
+    def test_index(self):
+        resp = self.client.get('/blog/')
+        self.assertEqual(resp.status_code, 200)
