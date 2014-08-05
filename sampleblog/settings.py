0a13447539674eda25a925c60e7a4857846d52e8
Yashwanth <yashwanth@agiliq.com>
2014-08-05 15:21:03 +0530

    Commit of adding environment variables

--- sampleblog/settings.py
+++ sampleblog/settings.py
@@ -133,8 +133,8 @@ BLOG_NUMBER_OF_ENTRIES_PER_PAGE = 3
 BLOG_NUMBER_OF_RECENT_ENTRIES_PER_PAGE = 5
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'yashwanthbabu.gujarathi@gmail.com'
-EMAIL_HOST_PASSWORD = 'yashwanthgujarathi'
+EMAIL_HOST = os.environ.get("emailhost")
+EMAIL_HOST_USER = os.environ.get("hostuser")
+EMAIL_HOST_PASSWORD = os.environ.get("hostpswd")
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_PORT = os.environ.get("emailport")
