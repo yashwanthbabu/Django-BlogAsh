b583eb5862a4f3ad53033e01e58226395964ddc5
Yashwanth <yashwanth@agiliq.com>
2014-08-06 10:52:38 +0530

    Commit of group imports

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,19 @@
 from django.forms import ModelForm
-from .models import Comment
 from django import forms
+
+from .models import Comment
+
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         exclude = ["post"]
-        
-        
+
+
 class CommentsForm(forms.Form):
     name = forms.CharField(max_length=32)
-    comment = forms.CharField(widget=forms.Textarea(attrs={'cols': 30, 'rows': 4}))
+    comment = forms.CharField(widget=forms.Textarea(attrs={'cols': 30,
+                                                    'rows': 4}))
 
 
 class PostForm(forms.Form):
