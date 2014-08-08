518790d1ebe62759b068b286548d1fb8d0e00fc9
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-08-08 15:07:06 +0530

    Deleting single comment

--- blog/forms.py
+++ blog/forms.py
@@ -8,13 +8,16 @@ class CommentForm(ModelForm):
     class Meta:
         model = Comment
         exclude = ["post"]
-    
+
 
 class CommentsForm(forms.Form):
-    Author = forms.CharField(widget=forms.TextInput(attrs={'size':'48', 'class':'form-control'}))
+    Author = forms.CharField(widget=forms.TextInput(attrs={'size': '48',
+                             'class': 'form-control'}))
     # A CharField that checks that the value is a valid email address.
-    email = forms.EmailField(widget=forms.TextInput(attrs={'size':'48', 'class':'form-control'}))
-    Body = forms.CharField(widget=forms.Textarea(attrs={'cols':50, 'rows': 5 , 'class':'form-control'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'size': '48',
+                             'class': 'form-control'}))
+    Body = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5,
+                           'class': 'form-control'}))
 
 
 class PostForm(forms.Form):
