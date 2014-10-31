94d8d1dd20ba9fdfdb9049aceda9c3d06406f58d
yashwanthbabu <yashwanthbabu.gujarathi@gmail.com>
2014-10-31 14:20:19 +0530

    Adding environment variable to email backend settings

--- sampleblog/settings.py
+++ sampleblog/settings.py
@@ -215,9 +215,9 @@ ACCOUNT_SETTINGS_REDIRECT_URL = 'home'
 ACCOUNT_EMAIL_CONFIRMATION_EXPIRE_DAYS = 7
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "yashwanth@agiliq.com"
-EMAIL_HOST_PASSWORD = "yashwanthgujarathi"
+EMAIL_HOST = os.environ.get('emailhost')
+EMAIL_HOST_USER = os.environ.get('hostuser')
+EMAIL_HOST_PASSWORD = os.environ.get('hostpswd')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
